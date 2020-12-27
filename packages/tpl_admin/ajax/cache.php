7ec340fc74767e6ba40623088076b162ab2688b3
Denys <denys@joomla-ua.org>
2020-12-27 18:25:41 +0200

    Update ajax feedback (cache and tags)

--- packages/tpl_admin/ajax/cache.php
+++ packages/tpl_admin/ajax/cache.php
@@ -6,67 +6,88 @@
  * @subpackage       admin
  *
  * @author           Denys Nosov, denys@joomla-ua.org
- * @copyright        2018-2019 (C) Joomla! Ukraine, https://joomla-ua.org. All rights reserved.
+ * @copyright        2018-2020 (C) Joomla! Ukraine, https://joomla-ua.org. All rights reserved.
  * @license          Creative Commons Attribution-Noncommercial-No Derivative Works 3.0 License (http://creativecommons.org/licenses/by-nc-nd/3.0/)
  */
 
 error_reporting(0);
 ini_set('display_errors', 0);
 
-$root = $_SERVER[ 'DOCUMENT_ROOT' ];
+echo (new cache())->run();
 
-echo unlinkRecursive($root . '/cache/com_cck**', '1');
-
-if( is_dir($root . '/cache/com_home/') )
+class cache
 {
-	echo unlinkRecursive($root . '/cache/com_home/', '1');
-}
-
-/**
- * @param      $dir
- * @param null $deleteRootToo
- *
- * @return string
- *
- * @since 1.0
- */
-function unlinkRecursive($dir, $deleteRootToo = null)
-{
-	$folders = glob($dir, GLOB_BRACE);
-
-	$notification = [];
-	foreach( $folders as $folder )
+	/**
+	 * @since 1.0
+	 */
+	public function __construct()
 	{
-		if( !$dh = opendir($folder) )
-		{
-			return false;
-		}
-
-		while( false !== ($obj = readdir($dh)) )
-		{
-			if( $obj === '.' || $obj === '..' )
-			{
-				continue;
-			}
-
-			$notification[] = '<span uk-icon="icon: check"></span> ' . trim(str_replace($_SERVER[ 'DOCUMENT_ROOT' ] . '/cache/', '', $folder), '/') . '<br>';
-
-			if( !unlink($folder . '/' . $obj) )
-			{
-				unlinkRecursive($folder . '/' . $obj, true);
-			}
-		}
-
-		closedir($dh);
-
-		if( $deleteRootToo === '1' )
-		{
-			rmdir($folder);
-		}
+		$this->root = dirname(__DIR__, 3);
 	}
 
-	$notify = array_unique($notification);
-	$notify = implode($notify);
+	/**
+	 * @return string
+	 *
+	 * @since 1.0
+	 */
+	public function run()
+	{
+		$html = $this->unlinkRecursive($this->root . '/cache/com_cck**', '1');
 
-	return $notify;
+		if(is_dir($this->root . '/cache/com_home/'))
+		{
+			$html .= $this->unlinkRecursive($this->root . '/cache/com_home/', '1');
+		}
+
+		return $html;
+	}
+
+	/**
+	 * @param      $dir
+	 * @param null $deleteRootToo
+	 *
+	 * @return string
+	 *
+	 * @since 1.0
+	 */
+	private function unlinkRecursive($dir, $deleteRootToo = null)
+	{
+		$folders = glob($dir, GLOB_BRACE);
+
+		$notification = [];
+		foreach($folders as $folder)
+		{
+			if(!$dh = opendir($folder))
+			{
+				return false;
+			}
+
+			while(false !== ($obj = readdir($dh)))
+			{
+				if($obj === '.' || $obj === '..')
+				{
+					continue;
+				}
+
+				$notification[] = '<span data-uk-icon="icon: check"></span> ' . trim(str_replace($this->root . '/cache/', '', $folder), '/') . '<br>';
+
+				if(!unlink($folder . '/' . $obj))
+				{
+					$this->unlinkRecursive($folder . '/' . $obj, true);
+				}
+			}
+
+			closedir($dh);
+
+			if($deleteRootToo === '1')
+			{
+				rmdir($folder);
+			}
+		}
+
+		$notify = array_unique($notification);
+		$notify = implode($notify);
+
+		return $notify;
+	}
 }
--- packages/tpl_admin/ajax/tags.php
+++ packages/tpl_admin/ajax/tags.php
@@ -6,85 +6,108 @@
  * @subpackage       admin
  *
  * @author           Denys Nosov, denys@joomla-ua.org
- * @copyright        2018-2019 (C) Joomla! Ukraine, https://joomla-ua.org. All rights reserved.
+ * @copyright        2018-2020 (C) Joomla! Ukraine, https://joomla-ua.org. All rights reserved.
  * @license          Creative Commons Attribution-Noncommercial-No Derivative Works 3.0 License (http://creativecommons.org/licenses/by-nc-nd/3.0/)
  */
+
+error_reporting(0);
+ini_set('display_errors', 0);
 
 header('Content-type: application/json; charset=utf-8');
 
 define('_JEXEC', 1);
-define('JPATH_BASE', $_SERVER[ 'DOCUMENT_ROOT' ]);
+define('JPATH_BASE', dirname(__DIR__, 3));
 
 require_once JPATH_BASE . '/includes/defines.php';
 require_once JPATH_BASE . '/includes/framework.php';
 
-error_reporting(1);
-ini_set('display_errors', 1);
-
 use Joomla\CMS\Factory;
 
-$_app = Factory::getApplication('site');
-$user = Factory::getUser();
+$app    = Factory::getApplication('site');
+$app->loadDispatcher();
 
-$_app->initialise();
+echo (new tags())->run();
 
-if($user->get('id') < 1)
+class tags
 {
-	return;
-}
-
-$app    = Factory::getApplication();
-$search = $app->input->getString('term');
-
-if(isset($search))
-{
-	$db    = Factory::getDbo();
-	$query = $db->getQuery(true);
-
-	$query->select([ 'a.metakey' ]);
-	$query->from($db->quoteName('#__content') . ' AS a');
-	$query->where(($db->quoteName('a.metakey') . ' LIKE ' . $db->quote('%' . $search . '%') . ' OR ' . $db->quoteName('a.metakey') . ' LIKE ' . $db->quote($search . '%')));
-	$query->group('a.metakey');
-	$query->order('a.metakey ASC');
-	$db->setQuery($query, 0, 30);
-	$rows = $db->loadObjectList();
-
-	sort($rows);
-	$data = [];
-	foreach($rows as $mod)
+	/**
+	 * @throws \Exception
+	 * @since 1.0
+	 */
+	public function __construct()
 	{
-		sort($arr);
-		$arr = explode(',', $mod->metakey);
-		$i   = 0;
-		foreach($arr as $row)
+		$this->user   = Factory::getUser();
+		$this->app    = Factory::getApplication();
+		$this->db     = Factory::getDbo();
+		$this->search = $this->app->input->getString('term');
+	}
+
+	/**
+	 * @return string
+	 *
+	 * @since 1.0
+	 */
+	public function run()
+	{
+
+		if($this->user->id < 1)
 		{
-			if(trim($row) != '' && $i <= 15)
+			return false;
+		}
+
+		if(isset($this->search))
+		{
+			$query = $this->db->getQuery(true);
+
+			$query->select([ 'a.metakey' ]);
+			$query->from($this->db->quoteName('#__content') . ' AS a');
+			$query->where(($this->db->quoteName('a.metakey') . ' LIKE ' . $this->db->quote('%' . $this->search . '%') . ' OR ' . $this->db->quoteName('a.metakey') . ' LIKE ' . $this->db->quote($this->search . '%')));
+			$query->group('a.metakey');
+			$query->order('a.metakey ASC');
+			$this->db->setQuery($query, 0, 30);
+			$rows = $this->db->loadObjectList();
+
+			sort($rows);
+			$data = [];
+			foreach($rows as $mod)
 			{
-				$pos = strpos(mb_strtolower(trim($row)), mb_strtolower($search));
-				if($pos !== false)
+				sort($arr);
+				$arr = explode(',', $mod->metakey);
+				$i   = 0;
+				foreach($arr as $row)
 				{
-					$str    = trim($row);
-					$str    = str_replace([
-						'"',
-						'»',
-						'«',
-						'”',
-						'“'
-					], '', $str);
-					$data[] = $str;
+					if(trim($row) != '' && $i <= 15)
+					{
+						$pos = strpos(mb_strtolower(trim($row)), mb_strtolower($this->search));
+						if($pos !== false)
+						{
+							$str    = trim($row);
+							$str    = str_replace([
+								'"',
+								'»',
+								'«',
+								'”',
+								'“'
+							], '', $str);
+							$data[] = $str;
+						}
+					}
+
+					$i++;
 				}
 			}
-			$i++;
+
+			$inputs = array_unique($data, SORT_LOCALE_STRING);
+
+			$keyword = [];
+			foreach($inputs as $k => $v)
+			{
+				$keyword[] = $v;
+			}
+
+			return json_encode($keyword);
 		}
+
+		return false;
 	}
-
-	$inputs = array_unique($data, SORT_LOCALE_STRING);
-
-	$keyword = [];
-	foreach($inputs as $k => $v)
-	{
-		$keyword[] = $v;
-	}
-
-	echo json_encode($keyword);
 }
