149f751352da4675bd8ac9594a905cf9c5c826b0
Denys <denys@joomla-ua.org>
2020-12-28 17:34:41 +0200

    Fix files

--- packages/file_lib/variations/form/form.php
+++ packages/file_lib/variations/form/form.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Admin Template
+ *
+ * @package          Joomla.Site
+ * @subpackage       admin
+ *
+ * @author           Denys Nosov, denys@joomla-ua.org
+ * @copyright        2018-2020 (C) Joomla! Ukraine, https://joomla-ua.org. All rights reserved.
+ * @license          GNU General Public License version 2 or later
+ */
 
 defined('_JEXEC') or die;
 
