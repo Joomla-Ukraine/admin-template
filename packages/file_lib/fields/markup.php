3cc27a478e3f7413992a8ea2e7c4e0aaab831229
Denys <denys@joomla-ua.org>
2023-07-20 15:54:09 +0300

    Update templates

--- packages/file_lib/fields/markup.php
+++ packages/file_lib/fields/markup.php
@@ -1,36 +1,26 @@
 <?php
 /**
- * Admin Template
+ * JU 2col
  *
  * @package          Joomla.Site
- * @subpackage       admin
+ * @subpackage       JU 2col
  *
  * @author           Denys Nosov, denys@joomla-ua.org
- * @copyright        2018-2023 (C) Joomla! Ukraine, https://joomla-ua.org. All rights reserved.
- * @license          GNU General Public License version 2 or later
+ * @copyright        2019-2021 (C) Joomla! Ukraine, http://joomla-ua.org. All rights reserved.
+ * @license          GNU General Public License version 2 or later; see _LICENSE.php
  */
-
-/**
- * @version            SEBLOD 3.x Core
- * @package            SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
- * @url                http://www.seblod.com
- * @editor             Octopoos - www.octopoos.com
- * @copyright          Copyright (C) 2009 - 2016 SEBLOD. All Rights Reserved.
- * @license            GNU General Public License version 2 or later; see _LICENSE.php
- **/
-
-/** The markup around each field (label+value/form) can be Overridden.
- * Remove the underscore [_] from the Filename. (filename = markup.php)
- * Edit the function name:
- *    - fields/markup.php            =>    cckMarkup_[template]
- *    - fields/[contenttype]/markup.php    =>    cckMarkup_[template]_[contenttype]
- *    - fields/[searchtype]/markup.php    =>    cckMarkup_[template]_[searchtype]
- * Write your Custom Markup code. (see default markup below)
- **/
 
 use Joomla\CMS\Factory;
 
 defined('_JEXEC') or die;
+
+// The markup around each field (label+value/form) can be Overridden.
+// Remove the underscore [_] from the Filename. (filename = markup.php)
+// Edit the function name:
+//	- fields/markup.php 			=>	cckMarkup_[template]
+//	- fields/[contenttype]/markup.php	=>	cckMarkup_[template]_[contenttype]
+//	- fields/[searchtype]/markup.php	=>	cckMarkup_[template]_[searchtype]
+// Write your Custom Markup code. (see default markup below)
 
 /**
  * @param $cck
@@ -41,7 +31,23 @@ defined('_JEXEC') or die;
  * @return mixed|string
  *
  * @throws \Exception
- * @since 2.0
+ * @since 1.5
+ */
+function cckMarkup_ju_2col($cck, $html, $field, $options)
+{
+	return cckMarkup_seb_minima($cck, $html, $field, $options);
+}
+
+/**
+ * @param $cck
+ * @param $html
+ * @param $field
+ * @param $options
+ *
+ * @return mixed|string
+ *
+ * @throws \Exception
+ * @since 1.5
  */
 function cckMarkup_seb_one($cck, $html, $field, $options)
 {
@@ -57,7 +63,7 @@ function cckMarkup_seb_one($cck, $html, $field, $options)
  * @return mixed|string
  *
  * @throws \Exception
- * @since 2.0
+ * @since 1.5
  */
 function cckMarkup_seb_minima($cck, $html, $field, $options)
 {
@@ -66,11 +72,13 @@ function cckMarkup_seb_minima($cck, $html, $field, $options)
 		return $html;
 	}
 
+	// Computation
 	if(isset($field->computation) && $field->computation)
 	{
 		$cck->setComputationRules($field);
 	}
 
+	// Conditional
 	if(isset($field->conditional) && $field->conditional)
 	{
 		$cck->setConditionalStates($field);
@@ -82,137 +90,159 @@ function cckMarkup_seb_minima($cck, $html, $field, $options)
 	{
 		include_once JPATH_ROOT . '/libraries/cck/rendering/fields/markup.php';
 
-		return cckMarkup($cck, $html, $field, $options);
+		$html = cckMarkup($cck, $html, $field, $options);
 	}
-
-	$desc = '';
-	if($cck->getStyleParam('field_description', 0) && $field->description != '')
+	else
 	{
-		$desc = '<div class="uk-article-meta">' . strip_tags($field->description, '<br><a>') . '</div>';
-	}
-
-	$label = '';
-	if($options->get('field_label', $cck->getStyleParam('field_label', 1)))
-	{
-		$label = $cck->getLabel($field->name, true, ($field->required ? '<span class="uk-text-danger">*</span>' : ''));
-		$label = str_replace('<label', '<label class="uk-form-label"', $label);
-		$label = ($label != '') ? $label : '';
-	}
-
-	if(stripos($field->attributes, 'data-form-icon') !== false)
-	{
-		$attr  = $field->attributes;
-		$attrs = explode(' ', trim($attr));
-		foreach($attrs as $attrib)
+		$desc = '';
+		if($cck->getStyleParam('field_description', 0) && $field->description != '')
 		{
-			if(stripos($attrib, 'data-form-icon') !== false)
-			{
-				$vals = explode('=', trim($attrib));
-				$icon = str_replace('"', '', $vals[ 1 ]);
-
-				$flip = '';
-				if($vals[ 0 ] === 'data-form-icon-flip')
-				{
-					$flip = ' uk-form-icon-flip';
-				}
-			}
+			$desc = '<div class="uk-article-meta">' . strip_tags($field->description, '<br><a>') . '</div>';
 		}
 
-		$html = '<div id="' . $cck->id . '_' . $cck->mode_property . '_' . $field->name . '" class="uk-form-icon' . $flip . '"><i class="fa fa-' . $icon . '"></i>' . $html . '</div>';
+		$label = '';
+		if($options->get('field_label', $cck->getStyleParam('field_label', 1)))
+		{
+			$label = $cck->getLabel($field->name, true, ($field->required ? '<span class="uk-text-danger">*</span>' : ''));
+			$label = ($label != '') ? '<label class="uk-form-label">' . $label . '</label>' : '';
+		}
+
+		$html = '<div id="' . $cck->id . '_' . $cck->mode_property . '_' . $field->name . '" class="' . $field->markup_class . '">' . $html . '</div>';
+
+		//echo $field->type .'<br>';
+
+		switch($field->type)
+		{
+			case 'upload_image':
+				$html = str_replace([
+					'inputbox text',
+					'cck_label'
+				], [
+					'uk-input uk-width-4-5@m',
+					'uk-margin-top uk-form-label'
+				], $html);
+				$html = '<div class="uk-background-muted uk-card uk-card-body uk-card-small">' . $html . '</div>';
+				break;
+
+			case 'upload_file':
+				$_html = $field->form;
+
+				$_html = str_replace([
+					'inputbox text',
+					'cck_label'
+				], [
+					'uk-file uk-width-4-5@m',
+					'uk-margin-top'
+				], $_html);
+
+				$html = '<div class="uk-background-muted uk-card uk-card-body uk-card-small">' . $_html . '</div>';
+				break;
+
+			case 'link':
+				$_html = $field->form;
+
+				$_html = str_replace([
+					'inputbox text',
+					'cck_label'
+				], [
+					'uk-input',
+					'uk-margin-top'
+				], $_html);
+
+				$html = '<div class="uk-background-muted uk-card uk-card-body uk-card-small"><div class="uk-grid" data-uk-grid>' . $_html . '</div></div>';
+				break;
+
+			case 'textarea':
+				$html = str_replace('inputbox ', 'uk-textarea ', $field->form);
+				break;
+
+			case 'select_simple':
+			case 'select_dynamic':
+			case 'juselect_dynamic':
+			case 'select_dynamic_cascade':
+				$html = str_replace('inputbox ', 'uk-select ', $field->form);
+				break;
+
+			case 'checkbox_dynamic':
+				preg_match_all('#<fieldset.*?class="checkboxes(.*?)">(.*?)</fieldset>#is', $field->form, $out);
+
+				$vertical = '';
+				if(count($out[ 1 ]) && trim($out[ 1 ][ 0 ]) === 'vertical')
+				{
+					$vertical = '<br>';
+				}
+
+				$html = preg_replace('#<fieldset.*?class="checkboxes(.*?)">(.*?)</fieldset>#is', '\\2', $field->form);
+
+				$html = preg_replace('#<input type="checkbox"(.*?)class="inputbox checkbox(.*?)".*?/><label(.*?)>(.*?)</label>#is', '<label\\3 class="uk-margin-right"><input class="uk-checkbox\\2" type="checkbox" \\1> \\4</label>' . $vertical, $html);
+				break;
+
+			case 'radio':
+				preg_match_all('#<fieldset.*?class="radios(.*?)">(.*?)</fieldset>#is', $field->form, $out);
+
+				$vertical = '';
+				if(count($out[ 1 ]) && trim($out[ 1 ][ 0 ]) === 'vertical')
+				{
+					$vertical = '<br>';
+				}
+
+				$html = preg_replace('#<fieldset.*?class="radios(.*?)">(.*?)</fieldset>#is', '\\2', $field->form);
+
+				$html = preg_replace('#<input type="radio"(.*?)class="radio(.*?)".*?/><label(.*?)>(.*?)</label>#is', '<label\\3 class="uk-margin-right"><input class="uk-radio\\2" type="radio" \\1> \\4</label>' . $vertical, $html);
+				break;
+
+			case 'group_x':
+
+				$pos = strpos($html, '<table');
+				if($pos === false)
+				{
+					$html = str_replace([
+						'cck_form cck_form_group_x',
+						'cck_forms'
+					], [
+						'uk-card tm-card-flat-border uk-margin uk-padding-small',
+						'uk-margin'
+					], $html);
+				}
+				else
+				{
+					$html = str_replace([
+						'<table border="0" cellpadding="0" cellspacing="0" class="table">',
+						'class="cck_cgx cck_cgx_button'
+					], [
+						'<table class="uk-table uk-table-small uk-table-divider uk-table-striped">',
+						'class="uk-width-small uk-text-nowrap uk-text-right cck_cgx cck_cgx_button'
+					], $html);
+				}
+
+				// Select
+				$html = str_replace('<select', '<select class="uk-select"', $html);
+
+				break;
+
+			case 'field_x':
+				$html = str_replace('inputbox text', 'uk-input', $html);
+
+				if($field->css)
+				{
+					$html = str_replace('adminformlist', 'adminformlist ' . $field->css, $html);
+				}
+
+				if($field->attributes)
+				{
+					$html = str_replace('id="sortable_', $field->attributes . ' id="sortable_', $html);
+				}
+
+				break;
+
+			default:
+				$html = str_replace('inputbox ', 'uk-input ', $html);
+				break;
+		}
+
+		$html = '<div id="' . $cck->id . '_' . $field->name . '" class="uk-margin' . ($field->markup_class != '' ? ' ' . $field->markup_class : '') . '">' . $label . '<div class="uk-form-controls">' . $html . $desc . '</div></div>';
+
 	}
-
-	$html = '<div id="' . $cck->id . '_' . $cck->mode_property . '_' . $field->name . '" class="uk-form-controls ' . $field->markup_class . '">' . $html . '</div>';
-
-	// Debuger
-	//echo $field->type .'<br>';
-
-	switch($field->type)
-	{
-		case 'upload_image':
-			$_html = $field->form;
-
-			$_html = str_replace([
-				'inputbox text',
-				'cck_label'
-			], [
-				'uk-width-4-5',
-				'uk-margin-top uk-form-label'
-			], $_html);
-
-			$html = '<div class="uk-background-muted uk-card uk-card-body uk-card-small">' . $_html . '</div>';
-			break;
-
-		case 'textarea':
-			$html = str_replace('inputbox ', 'uk-textarea ', $field->form);
-			break;
-
-		case 'select_simple':
-		case 'juselect_dynamic':
-		case 'select_dynamic':
-		case 'select_dynamic_cascade':
-			$html = str_replace('inputbox ', 'uk-select ', $field->form);
-			break;
-
-		case 'checkbox_dynamic':
-			preg_match_all('#<fieldset.*?class="checkboxes(.*?)">(.*?)</fieldset>#is', $field->form, $out);
-
-			$vertical = '';
-			if(count($out[ 1 ]) && trim($out[ 1 ][ 0 ]) == 'vertical')
-			{
-				$vertical = '<br>';
-			}
-
-			$html = preg_replace('#<fieldset.*?class="checkboxes(.*?)">(.*?)</fieldset>#is', '\\2', $field->form);
-
-			$html = preg_replace('#<input type="checkbox"(.*?)class="inputbox checkbox(.*?)".*?/><label(.*?)>(.*?)</label>#is', '<label\\3 class="uk-margin-right"><input class="uk-checkbox\\2" type="checkbox" \\1> \\4</label>' . $vertical, $html);
-			break;
-
-		case 'radio':
-			preg_match_all('#<fieldset.*?class="radios(.*?)">(.*?)</fieldset>#is', $field->form, $out);
-
-			$vertical = '';
-			if(count($out[ 1 ]) && trim($out[ 1 ][ 0 ]) == 'vertical')
-			{
-				$vertical = '<br>';
-			}
-
-			$html = preg_replace('#<fieldset.*?class="radios(.*?)">(.*?)</fieldset>#is', '\\2', $field->form);
-
-			$html = preg_replace('#<input type="radio"(.*?)class="radio(.*?)".*?/><label(.*?)>(.*?)</label>#is', '<label\\3 class="uk-margin-right"><input class="uk-radio\\2" type="radio" \\1> \\4</label>' . $vertical, $html);
-			break;
-
-		case 'group_x':
-			$html = str_replace([
-				'cck_form cck_form_group_x',
-				'cck_forms'
-			], [
-				'uk-card tm-card-flat-border uk-margin uk-padding-small',
-				'uk-margin'
-			], $html);
-
-			break;
-
-		case 'field_x':
-			$html = str_replace('inputbox text', 'uk-input ', $html);
-
-			if($field->css)
-			{
-				$html = str_replace('adminformlist', 'adminformlist ' . $field->css, $html);
-			}
-
-			if($field->attributes)
-			{
-				$html = str_replace('id="sortable_', $field->attributes . ' id="sortable_', $html);
-			}
-
-			break;
-
-		default:
-			$html = str_replace('inputbox ', 'uk-input ', $field->form);
-			break;
-	}
-
-	$html = '<div id="' . $cck->id . '_' . $field->name . '" class="uk-margin' . ($field->markup_class != '' ? ' ' . $field->markup_class : '') . '">' . $label . '<div class="uk-form-controls">' . $html . $desc . '</div></div>';
 
 	return $html;
 }
