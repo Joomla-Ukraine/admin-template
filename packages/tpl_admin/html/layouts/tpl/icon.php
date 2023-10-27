c9f6fdcac8d77a0c3af11683ace364fadc95d2bd
Denys <denys@joomla-ua.org>
2023-10-27 06:25:03 +0300

    Update to 2.4.2

--- packages/tpl_admin/html/layouts/tpl/icon.php
+++ packages/tpl_admin/html/layouts/tpl/icon.php
@@ -16,7 +16,7 @@ use Joomla\CMS\Uri\Uri;
 
 $data = (object) $displayData;
 
-$v = '2.4.1';
+$v = '2.4.2';
 
 $size = 20;
 if(isset($data->size))
--- packages/tpl_admin/inc/head.php
+++ packages/tpl_admin/inc/head.php
@@ -18,7 +18,7 @@ use Joomla\CMS\Uri\Uri;
 
 defined('_JEXEC') or die;
 
-$v = '2.4.1';
+$v = '2.4.2';
 
 $app  = Factory::getApplication();
 $wa   = $this->getWebAssetManager();
